588bc0bd2e674bb9ae472f0d9cffe89a8e2824a9
PawelPalikarpau <35779130+PawelPalikarpau@users.noreply.github.com>
2018-02-02 18:58:06 +0100

    login form created

---
+++ MyProjectUI/LoginForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MyProjectUI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.registrateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(110, 66);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(160, 29);
+            this.passwordTextBox.TabIndex = 0;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(22, 34);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(57, 21);
+            this.emailLabel.TabIndex = 1;
+            this.emailLabel.Text = "E-mail:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.loginButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.loginButton.Location = new System.Drawing.Point(24, 113);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(119, 56);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(22, 69);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(79, 21);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(110, 31);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(160, 29);
+            this.emailTextBox.TabIndex = 4;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.exitButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.exitButton.Location = new System.Drawing.Point(149, 113);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(119, 56);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "If you do not have an account";
+            // 
+            // registrateButton
+            // 
+            this.registrateButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.registrateButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.registrateButton.Location = new System.Drawing.Point(87, 202);
+            this.registrateButton.Name = "registrateButton";
+            this.registrateButton.Size = new System.Drawing.Size(119, 56);
+            this.registrateButton.TabIndex = 7;
+            this.registrateButton.Text = "Registrate";
+            this.registrateButton.UseVisualStyleBackColor = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(293, 287);
+            this.Controls.Add(this.registrateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button registrateButton;
+    }
+}
+
